fe900c39595a4068f1d5faf52650d5ba724cd293
xygodcyx <1323943635@qq.com>
2024-06-02 23:16:45 +0800

    测试

---
+++ docs/edit.js
@@ -0,0 +1,56 @@
+(function (win) {
+	function isFunction(functionToCheck) {
+		return typeof functionToCheck === 'function';
+	}
+	function editDoc(event, vm, docEditBase) {
+		var docName = vm.route.file;
+
+		if (docName) {
+			var editLink = docEditBase + docName;
+			window.open(editLink);
+			event.preventDefault();
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	win.EditOnGithubPlugin = {};
+
+	function create(docBase, docEditBase, title) {
+		title = title || 'Edit on github';
+		docEditBase = docEditBase || docBase.replace(/\/blob\//, '/edit/');
+
+		function generateHeader(title) {
+			return (header = [
+				'<div style="overflow: auto">',
+				'<p style="float: right"><a style="text-decoration: underline; cursor: pointer"',
+				'onclick="EditOnGithubPlugin.onClick(event)">',
+				title,
+				'</a></p>',
+				'</div>',
+			].join(''));
+		}
+
+		return function (hook, vm) {
+			console.log(EditOnGithubPlugin);
+			win.EditOnGithubPlugin.onClick = function (event) {
+				editDoc(event, vm, docEditBase);
+			};
+
+			if (isFunction(title)) {
+				hook.afterEach(function (html) {
+					return generateHeader(title(vm.route.file)) + html;
+				});
+			} else {
+				var header = generateHeader(title);
+
+				hook.afterEach(function (html) {
+					return header + html;
+				});
+			}
+		};
+	}
+
+	win.EditOnGithubPlugin.create = create;
+})(window);
